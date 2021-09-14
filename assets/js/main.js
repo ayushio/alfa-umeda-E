b4d4eb00c3b1304c003abffa25209a9d81516210
tsubasa225 <85094451+tsubasa225@users.noreply.github.com>
2021-09-14 20:36:25 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,15 +18,15 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: 'OGAWAHIDEKI',
         },
         stage2: {
-          q1: 'いいい',
+          q1: '10月12日',
           // q2: 'えええ',
           // q3: 'おおお'
         },
         stage3: {
-          q1: 'ううう',
+          q1: '22:57',
           // q2: 'かかか',
           // q3: 'ききき',
         }
